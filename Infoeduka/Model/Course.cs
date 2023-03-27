6bd757f7dec78723c92a55e83347abb341f57b39
Nina Crnković <nina.crnkovic.zg@gmail.com>
2023-03-27 16:20:44 +0200

    Added class Course

---
+++ Infoeduka/Model/Course.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infoeduka.Model
+{
+    public class Course
+    {
+        private const char DEL = '#';
+        public Guid Id { get; private set; }
+        public string Name { get; set; } = "";
+        public string Code { get; set; } = "";
+        public int Ects { get; set; } = 0;
+        public List<Person> Lecturers { get; set; } = new List<Person>();
+
+        public Course()
+        {
+
+        }
+
+        public Course(Guid id, string name, string code, int ects, List<Person> lecturers)
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            Ects = ects;
+            Lecturers = lecturers;
+        }
+
+        public Course(string name, string code, int ects, List<Person> lecturers)
+        {
+            Id = new Guid();
+            Name = name;
+            Code = code;
+            Ects = ects;
+            Lecturers = lecturers;
+        }
+
+        public override string ToString() => $"{Id}, {Name}, {Code}, {Ects}, {Lecturers?.ToString()}";
+
+
+
+        public string FormatForFile()
+        {
+            string lecturers = Lecturers.Any() ? string.Join(DEL, Lecturers.Select(l => l.FormatForFile())) : "";
+
+            return $"{Id}{DEL}{Name}{DEL}{Code}{DEL}{Ects}{DEL}{lecturers}";
+
+
+        }
+        public static Course ParseFromFile(string line)
+        {
+            string[] parts = line.Split(DEL);
+            if (parts.Length < 5)
+                throw new ArgumentException("Invalid line format: " + line);
+
+            if (!Guid.TryParse(parts[0], out Guid id))
+                throw new ArgumentException("Invalid ID format: " + parts[0]);
+
+            var course = new Course
+            {
+                Id = id,
+                Name = parts[1],
+                Code = parts[2],
+                Ects = int.Parse(parts[3])
+            };
+
+
+            string[] peopleInfo = parts[4].Split(DEL);
+            foreach (string personInfo in peopleInfo)
+            {
+                Person lecturer = Person.ParseFromFile(personInfo);
+                if (lecturer != null)
+                    course.Lecturers.Add(lecturer);
+            }
+
+            return course;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Course course &&
+                   Id.Equals(course.Id) &&
+                   Name == course.Name &&
+                   Code == course.Code &&
+                   Ects == course.Ects &&
+                   EqualityComparer<List<Person>>.Default.Equals(Lecturers, course.Lecturers);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Code, Ects, Lecturers);
+        }
+    }
+}
--- Infoeduka/Model/Person.cs
+++ Infoeduka/Model/Person.cs
@@ -6,7 +6,91 @@ using System.Threading.Tasks;
 
 namespace Infoeduka.Model
 {
-    internal class Person
+    public class Person
     {
+        private const char DEL = '|';
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public bool IsAdmin { get; set; } = false;
+        public Person()
+        {
+
+        }
+        public Person(Guid id, string firstName, string lastName, string email, string password, bool isAdmin)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+
+        public Person(string firstName, string lastName, string email, string password, bool isAdmin)
+        {
+            Id = Guid.NewGuid(); ;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+
+
+
+        public override string ToString() => $"{Id}, {FirstName} {LastName}, {Email}, {Password}, {(IsAdmin ? "Administrator" : "Predavač")}";
+
+        public string FormatForFile()
+        => $"{Id}{DEL}{FirstName}{DEL}{LastName}{DEL}{Email}{DEL}{Password}{DEL}{IsAdmin}";
+
+
+        public static Person ParseFromFile(string line)
+        {
+            string[] details = line.Split(DEL);
+
+            if (details.Length < 6)
+            {
+                throw new ArgumentException("Invalid line format: " + line);
+            }
+            if (!Guid.TryParse(details[0], out Guid id))
+            {
+                throw new ArgumentException("Invalid ID format: " + details[0]);
+
+            }
+            return new Person
+            {
+                Id = id,
+                FirstName = details[1],
+                LastName = details[2],
+                Email = details[3],
+                Password = details[4],
+                IsAdmin = bool.Parse(details[5])
+            };
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Person person &&
+                   Id.Equals(person.Id) &&
+                   FirstName == person.FirstName &&
+                   LastName == person.LastName &&
+                   Email == person.Email &&
+                   Password == person.Password &&
+                   IsAdmin == person.IsAdmin;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, FirstName, LastName, Email, Password, IsAdmin);
+        }
+
+
+
     }
 }
