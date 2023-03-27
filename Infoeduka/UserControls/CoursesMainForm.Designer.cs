dcfcae5a17b687bdfc42ddbd585fcf0b1dc4d0e1
Nina Crnković <nina.crnkovic.zg@gmail.com>
2023-03-27 17:41:01 +0200

    Added usercontrol LecturerMainForm

--- Infoeduka/UserControls/CoursesMainForm.Designer.cs
+++ Infoeduka/UserControls/CoursesMainForm.Designer.cs
@@ -28,10 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.gbCourse = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnMainNotifications = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gbCourse.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbCourse
+            // 
+            this.gbCourse.Controls.Add(this.label5);
+            this.gbCourse.Controls.Add(this.label4);
+            this.gbCourse.Controls.Add(this.flowLayoutPanel2);
+            this.gbCourse.Controls.Add(this.flowLayoutPanel1);
+            this.gbCourse.Controls.Add(this.btnMainNotifications);
+            this.gbCourse.Controls.Add(this.panel3);
+            this.gbCourse.Controls.Add(this.textBox2);
+            this.gbCourse.Controls.Add(this.label2);
+            this.gbCourse.Controls.Add(this.panel2);
+            this.gbCourse.Controls.Add(this.textBox1);
+            this.gbCourse.Controls.Add(this.label1);
+            this.gbCourse.Controls.Add(this.panel1);
+            this.gbCourse.Controls.Add(this.tbEmail);
+            this.gbCourse.Controls.Add(this.label3);
+            this.gbCourse.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gbCourse.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.gbCourse.Location = new System.Drawing.Point(86, 14);
+            this.gbCourse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbCourse.Name = "gbCourse";
+            this.gbCourse.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbCourse.Size = new System.Drawing.Size(811, 488);
+            this.gbCourse.TabIndex = 3;
+            this.gbCourse.TabStop = false;
+            this.gbCourse.Text = "Kolegij";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(483, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 21);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Predavači na kolegiju";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(180, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 21);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Svi predavači";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(458, 188);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(219, 225);
+            this.flowLayoutPanel2.TabIndex = 21;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(134, 188);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(219, 225);
+            this.flowLayoutPanel1.TabIndex = 20;
+            // 
+            // btnMainNotifications
+            // 
+            this.btnMainNotifications.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(95)))), ((int)(((byte)(32)))));
+            this.btnMainNotifications.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnMainNotifications.FlatAppearance.BorderSize = 0;
+            this.btnMainNotifications.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(35)))), ((int)(((byte)(80)))));
+            this.btnMainNotifications.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMainNotifications.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMainNotifications.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnMainNotifications.Location = new System.Drawing.Point(234, 431);
+            this.btnMainNotifications.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMainNotifications.Name = "btnMainNotifications";
+            this.btnMainNotifications.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.btnMainNotifications.Size = new System.Drawing.Size(342, 34);
+            this.btnMainNotifications.TabIndex = 19;
+            this.btnMainNotifications.Text = "Spremi";
+            this.btnMainNotifications.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Location = new System.Drawing.Point(354, 137);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(262, 1);
+            this.panel3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(23)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.Location = new System.Drawing.Point(354, 118);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(194, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Broj ECTS bodova";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Location = new System.Drawing.Point(354, 88);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(262, 1);
+            this.panel2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(23)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(354, 68);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(232, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Šifra kolegija";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Location = new System.Drawing.Point(354, 44);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 1);
+            this.panel1.TabIndex = 5;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(23)))));
+            this.tbEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbEmail.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.tbEmail.Location = new System.Drawing.Point(354, 25);
+            this.tbEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(262, 22);
+            this.tbEmail.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(223, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Naziv kolegija";
+            // 
+            // CoursesMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(23)))));
+            this.Controls.Add(this.gbCourse);
+            this.Name = "CoursesMainForm";
+            this.Size = new System.Drawing.Size(982, 517);
+            this.gbCourse.ResumeLayout(false);
+            this.gbCourse.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox gbCourse;
+        private Label label5;
+        private Label label4;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnMainNotifications;
+        private Panel panel3;
+        private TextBox textBox2;
+        private Label label2;
+        private Panel panel2;
+        private TextBox textBox1;
+        private Label label1;
+        private Panel panel1;
+        private TextBox tbEmail;
+        private Label label3;
     }
 }
